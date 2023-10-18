c96ffcbd54d2ee14d5b214b48cc827ac080f97d5
Samuel Berthe <dev@samuel-berthe.fr>
2023-10-18 02:46:04 +0200

    feat: max size for http body dump

--- dump.go
+++ dump.go
@@ -8,18 +8,24 @@ import (
 
 type bodyWriter struct {
 	gin.ResponseWriter
-	body *bytes.Buffer
+	body    *bytes.Buffer
+	maxSize int
 }
 
 // implements gin.ResponseWriter
 func (w bodyWriter) Write(b []byte) (int, error) {
-	w.body.Write(b)
+	if w.body.Len()+len(b) > w.maxSize {
+		w.body.Write(b[:w.maxSize-w.body.Len()])
+	} else {
+		w.body.Write(b)
+	}
 	return w.ResponseWriter.Write(b)
 }
 
-func newBodyWriter(writer gin.ResponseWriter) *bodyWriter {
+func newBodyWriter(writer gin.ResponseWriter, maxSize int) *bodyWriter {
 	return &bodyWriter{
 		body:           bytes.NewBufferString(""),
 		ResponseWriter: writer,
+		maxSize:        maxSize,
 	}
 }
