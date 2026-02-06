ad4c0155dae791a088f1fcbd44be5b5b1b9d17ee
Samuel Berthe <dev@samuel-berthe.fr>
2026-02-06 18:17:07 +0100

    Simplify bodyWriter write method using io.Copy
    Refactor write handling in bodyWriter to use io.Copy for simplicity.

--- dump.go
+++ dump.go
@@ -14,8 +14,6 @@ var _ http.Flusher = (*bodyWriter)(nil)
 var _ http.Hijacker = (*bodyWriter)(nil)
 var _ io.ReaderFrom = (*bodyWriter)(nil)
 
-var errInvalidWrite = errors.New("invalid write result")
-
 type bodyWriter struct {
 	gin.ResponseWriter
 	body    *bytes.Buffer
@@ -49,54 +47,8 @@ func (w *bodyWriter) ReadFrom(r io.Reader) (int64, error) {
 			return n, err
 		}
 	}
-	// inline io.Copy(w, r) without ReaderFrom interface usage
-	if wt, ok := r.(io.WriterTo); ok {
-		return wt.WriteTo(w)
-	}
 
-	size := 32 * 1024
-	if l, ok := r.(*io.LimitedReader); ok && int64(size) > l.N {
-		if l.N < 1 {
-			size = 1
-		} else {
-			size = int(l.N)
-		}
-	}
-	buf := make([]byte, size)
-
-	var (
-		written int64
-		err     error
-	)
-	for {
-		nr, er := r.Read(buf)
-		if nr > 0 {
-			nw, ew := w.Write(buf[0:nr])
-			if nw < 0 || nr < nw {
-				nw = 0
-				if ew == nil {
-					ew = errInvalidWrite
-				}
-			}
-			written += int64(nw)
-			if ew != nil {
-				err = ew
-				break
-			}
-			if nr != nw {
-				err = io.ErrShortWrite
-				break
-			}
-		}
-		if er != nil {
-			if er != io.EOF {
-				err = er
-			}
-			break
-		}
-	}
-
-	return written, err
+	return io.Copy(struct{ io.Writer }{w}, r)
 }
 
 func newBodyWriter(writer gin.ResponseWriter, maxSize int, recordBody bool) *bodyWriter {
